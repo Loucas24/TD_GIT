665a018f5b212e0ea2369cc3d0d491cc7865920c
Lucas Verron <luk.verron24@gmail.com>
2025-10-13 14:03:33 +0200

    cc

--- test.c
+++ test.c
@@ -2,7 +2,21 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello");
-    printf("coucou");
+    int seconde = 0;
+    int heure = 0;
+    int minute = 0;
+    scanf("%d",&seconde);
+    while (seconde > 3600)
+    {
+        heure = heure + 1;
+        seconde = seconde - 3600;
+    }
+    while (seconde > 60)
+    {
+        minute = minute + 1;
+        seconde = seconde - 60;
+    }
+    
+    printf("%d secondes = %d heures, %d minutes et %d secondes" , heure,minute,seconde);
     return 0;
 }
